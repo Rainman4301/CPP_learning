630318fc585d13732cf58780e972b52052867166
Rainman4301 <wwwee6520@gmail.com>
2024-03-17 22:37:45 +1030

    decorator_virtual

---
+++ python_learning/decorator/decorator.py
@@ -0,0 +1,15 @@
+# Decorator function
+def print_args(func):
+    def wrapper(*args, **kwargs):
+        print("Arguments:", args)
+        print("Keyword arguments:", kwargs)
+        return func(*args, **kwargs)
+    return wrapper
+
+# Function decorated with @print_args
+@print_args
+def greet(name, age):
+    print(f"Hello {name}, you are {age} years old!")
+
+# Call the decorated function
+greet("Alice", age=30)
